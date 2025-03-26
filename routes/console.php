4db77d238654ee298488bb4b94cdd040e09790c0
p0wered <powered.ui@gmail.com>
2025-03-27 01:31:37 +0300

    changes in this commit: - hotfix

--- routes/console.php
+++ routes/console.php
@@ -3,24 +3,27 @@
 use App\Models\ImportSetting;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-$settings = ImportSetting::first();
+if (Schema::hasTable('import_settings')) {
+    $settings = ImportSetting::first();
 
-if ($settings) {
-    $frequency = $settings->excel_import_frequency;
-    $day = $settings->excel_import_day;
-    $time = $settings->excel_import_time;
+    if ($settings) {
+        $frequency = $settings->excel_import_frequency;
+        $day = $settings->excel_import_day;
+        $time = $settings->excel_import_time;
 
-    if ($frequency == 'weekly') {
-        Schedule::command('import:excel')->weeklyOn((int) $day, $time)->withoutOverlapping();
-    } else if ($frequency == 'daily') {
-        Schedule::command('import:excel')->dailyAt($time)->withoutOverlapping();
-    } else if ($frequency == 'monthly') {
-        Schedule::command('import:excel')->monthlyOn((int) $day, $time)->withoutOverlapping();
+        if ($frequency == 'weekly') {
+            Schedule::command('import:excel')->weeklyOn((int) $day, $time)->withoutOverlapping();
+        } else if ($frequency == 'daily') {
+            Schedule::command('import:excel')->dailyAt($time)->withoutOverlapping();
+        } else if ($frequency == 'monthly') {
+            Schedule::command('import:excel')->monthlyOn((int) $day, $time)->withoutOverlapping();
+        }
     }
 }
