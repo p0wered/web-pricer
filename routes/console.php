4954c941e7d0afa5c1f8c29caef270717cc0fd14
p0wered <powered.ui@gmail.com>
2025-03-27 21:37:55 +0300

    changes in this commit: - another hotfix

--- routes/console.php
+++ routes/console.php
@@ -5,25 +5,32 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-if (Schema::hasTable('import_settings')) {
-    $settings = ImportSetting::first();
+try {
+    DB::connection()->getPdo();
 
-    if ($settings) {
-        $frequency = $settings->excel_import_frequency;
-        $day = $settings->excel_import_day;
-        $time = $settings->excel_import_time;
+    if (Schema::hasTable('import_settings')) {
+        $settings = ImportSetting::first();
 
-        if ($frequency == 'weekly') {
-            Schedule::command('import:excel')->weeklyOn((int) $day, $time)->withoutOverlapping();
-        } else if ($frequency == 'daily') {
-            Schedule::command('import:excel')->dailyAt($time)->withoutOverlapping();
-        } else if ($frequency == 'monthly') {
-            Schedule::command('import:excel')->monthlyOn((int) $day, $time)->withoutOverlapping();
+        if ($settings) {
+            $frequency = $settings->excel_import_frequency;
+            $day = $settings->excel_import_day;
+            $time = $settings->excel_import_time;
+
+            if ($frequency == 'weekly') {
+                Schedule::command('import:excel')->weeklyOn((int) $day, $time)->withoutOverlapping();
+            } else if ($frequency == 'daily') {
+                Schedule::command('import:excel')->dailyAt($time)->withoutOverlapping();
+            } else if ($frequency == 'monthly') {
+                Schedule::command('import:excel')->monthlyOn((int) $day, $time)->withoutOverlapping();
+            }
         }
     }
+} catch (\Exception $e) {
+    // do nothing
 }
